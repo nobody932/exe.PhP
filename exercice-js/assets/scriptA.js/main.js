3f1fb6af1215e95a95cbe8260e8f1c6fb4688ed0
nobody932 <haykel93370@gmail.com>
2023-07-11 16:43:32 +0200

    update

--- exercice-js/assets/scriptA.js/main.js
+++ exercice-js/assets/scriptA.js/main.js
@@ -14,3 +14,26 @@ const expr = (Fruit) => {
 };
 
 expr("Fraise");
+
+/*Challenge JS : Créer une fonction showName avec deux paramètres : firstName et lastName. La fonction doit renvoyer dans la console > Bonjour, je m'appelle FIRSTNAME et LASTNAME.*/
+
+
+const showName = (firstName,lastName) => {
+console.log(`bonjour,je m'appelle ${firstName} ${lastName}`);
+
+}
+
+showName();
+
+
+
+/*Challenge JS : Créer une fonction Multiply ayant un seul argument permettant de multiplier le nombre par lui même.
+
+Exemple : lors de l'appel à la fonction, dans la console, l'argument 5 aura pour résultat 5 x 5 qui fait donc 25.*/
+
+
+const Multiply =(number)=>{
+console.log(number*number)
+
+}
+Multiply(5)
