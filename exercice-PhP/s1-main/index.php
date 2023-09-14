1d5001a13b029dfb49c295773d7bed73927b6868
nobody932 <haykel93370@gmail.com>
2023-09-14 09:36:43 +0200

    update

--- exercice-PhP/s1-main/index.php
+++ exercice-PhP/s1-main/index.php
@@ -1,5 +1,5 @@
 <!-- 
-Dans cet exercice, vous apprendrez à manipuler les tableaux php.
+Dans cet exercice, vous apprendrez à manipuler la syntaxe php.
 Veillez à bien lire les instructions présentes en commentaires.
 
 Good luck & happy coding !
@@ -13,93 +13,104 @@ Good luck & happy coding !
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Made with ❤️ by HelloDev</title>
-    <link rel="stylesheet" href="./assets/css/styles.css">
-    <script src="./assets/js/main.js"></script>
+    <!-- Ici, réaliser un lien avec le fichier styles.css -->
+    <link rel="stylesheet" href="./assets/css/styles.css" />
+    <!-- Ici, réaliser un lien avec le fichier main.js sans oublier l'attribut DEFER ou ASYNC -->
+    <script src="./assets/js/main.js" defer></script>
 </head>
 
 <body>
-    <h1 class="text-center py-10">Learn PHP - Step 1</h1>
+    <h1 class="text-center py-10">Learn PHP - Step 0</h1>
+    <!-- Ici, réaliser une ouverture et une fermeture de balise de type PHP -->
+    <!-- Dans la balise PHP, déclarer une variable NAME ayant pour valeur votre prénom. Ensuite, effectuer un print grâce à la méthode ECHO 
+    Exemple : John-->
+    <?php
+    $name = 'haykel';
+    echo "$name";
 
-    <!-- Step 1.1 -->
-    <!-- Déclarer une variable MY PRODUCT de type tableau en SNAKE CASE
+    $lastname ='ALERTE';
+    print "$lastname";
+
+    echo "<br> Je m'appelle $NAME $LASTNAME <br>";
+    echo "Je m'appelle {$NAME}  {$LASTNAME} <br>"; 
+    // FAUX
+    ?>
+
+    <!-- Step 0.1 -->
+    <!-- Déclarer une variable LASTNAME ayant pour valeur votre nom de famille puis effectuer une concaténation avec NAME. Enfin, faites un print avec la méthode ECHO.
+    Exemple : John Doe -->
+
+      <!-- Step 0.2 -->
+    <!-- Déclarer des variables avec tous les types primitif en PHP.
+    --><?php
+    $age = 20;
+    $a_str ="foo";
+    $c = array("rouge","bleu","vert"); // SEMI FAUX
+    $vrai = true;
+    $faux = false; 
+    $vide = null;
+    $vide2;
+    echo "La variable $vide contient une valeur de type $faux <br>";
+    echo gettype($vrai);
+    
+    echo $age;
+    echo $vrai;
+     ?>
+  
+    <!-- Step 0.3 -->
+    <!-- Déclarer des variables avec tous les types primitif en PHP.
+    -->
+    <!-- Step 0.4 -->
+    <!-- Quelles types primitifs n'ont pas pu être déclarés ?
+    -->
+    bool   array <!-- FAUX -->
+    
+
+    <!-- Step 0.5 -->
+    <!-- Déclarer une variable X et une variable Y. Leurs types primitif est un nombre. Additionner les deux valeurs dans une variable SUM puis un print du total avec la méthode ECHO.
+    --> 
+    <?php 
+     $x = 2;
+     $y = 3;
+     $total = $y + $x;
+     echo "l'addition de {$x} et {$y} fais {$total}";
+         ?>
+    <!-- Step 0.6 -->
+    <!-- Déclarer une variable PRODUCTS. Le type primitif est un tableau. Ajouter plusieurs produits de votre choix en "dur". 
+    Afficher les informations de cette variable avec la méthode VAR_DUMP.
+    -->
+    <?php
+    $PRODUCTS= array(1=>'Boards','Trucks','Roues','Grip','Baskets'); // SEMI FAUX
+    var_dump($PRODUCTS);
+    ?>
+    <!-- Step 0.7 -->
+    <!-- Quelle est la différence entre les méthodes ECHO et VAR_DUMP ?
+    --> La méthode ECHO affiche une chaines de caractères (Les types primitifs) alors que la méthode VAR_DUMP affiche les détails d'un tableau en fournissant plus d'information
+
+    <!-- Step 0.8 -->
+    <!-- Déclarer une variable de type string. Afficher les informations de cette variable avec la méthode PRINT_R.
     -->
 <?php
-$MyProduct=['phone','pc','télévision'];
-function NewProduct(int $n,string $m )
-{
-    return ( '{$n}Ajouter un nouveau produit dans {$m}' );
-}
-echo $MyProduct;
-function camel_to_snake($str)
-{
-    return strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $str));
-}
-function snakeToCamel($str)
-{
-    return lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $str))));
-}
-$camel = 'CreateUserProfile';
-$snake = camel_to_snake($camel);
-echo 'to_snake: ' . $snake . "\n";
-$camel = snakeToCamel($snake);
-echo 'toCamel: ' . $camel . "\n";
-
-$stack = ['console', 'micro'];
-array_push($stack, 'écran', 'pc gamer');
-print_r($stack);
-$delete = array_pop($stack);
-print_r($stack);
-
-$replacements = array(0 => "casque", 4 => "tablette");
-$replacements2 = array(0 => "chargeur");
-$switch = array_replace($stack, $replacements, $replacements2);
-print_r($switch);
-?>
-    <!-- Step 1.2 -->
-    <!-- Dans la variable créée plus tôt, insérer 3 valeurs ou plus. Utiliser la méthode ARRAY_MAP avec la valeur NULL en premier argument.
-    -->
-
-    <!-- Step 1.3 -->
-    <!-- Avec la même variable, ajouter 1 valeur ou plus au tableau avec la méthode ARRAY_PUSH.
-    -->
-
-    <!-- Step 1.4 -->
-    <!-- Avec la même variable, supprimer la dernière valeur avec la méthode ARRAY_POP.
-    -->
-
-    <!-- Step 1.5 -->
-    <!-- Déclarer une nouvelle variable de type tableau en y insérant 3 valeurs ou plus. Remplacer l'ancien tableau de votre première variable par la deuxième avec la méthode ARRAY_REPLACE.
-    -->
-
-    <!-- Step 1.6 -->
-    <!-- A l'aide de vos deux variables, effectuer une fusion de deux tableaux grâce à la méthode ARRAY_MERGE.
-    -->
-
-    <!-- Step 1.7 -->
-    <!-- A l'aide de votre précédent tableau fusionné, utiliser la méthode ARRAY_RAND afin de choisir au hasard des valeurs à print.
-    -->
-
-    <!-- Step 1.8 -->
-    <!-- Déclarer une variable contenant 3 valeurs ou plus de type tableau associatif en textualisant les clés d'un tableau avec la méthode ARRAY. Ensuite, faites un print d'une valeur au choix en utilisant sa clé associée et la méthode PRINT_R ou ECHO.
-    -->
-
-    <!-- Step 1.9 -->
-    <!-- Avec le tableau associatif créé précédemment, remplacer la valeur d'une des clés avec la méthode ARRAY_REPLACE.
-    -->
-
-    <!-- Step 2 -->
-    <!--
-    Bravo, vous venez de terminer la manipulation d'un tableau PHP ! Ils existent encore pleins d'autres méthodes consultables ici :
-
-    https://www.php.net/manual/en/ref.array.php
     
-    Passons à la suite en vous rendant sur ce lien :
+$marks="<br> thrasher <br>";
+echo '<br> <br>';
+print_r($PRODUCTS);
+?>
+    <!-- Step 0.9 -->
+    <!-- 
+    PRINT_R est équivalent à ECHO ? oui
+    PRINT_R est équivalent à VAR_DUMP ? nan vus que VAR_DUMP donne plus d'information 
+    ECHO est équivalent à VAR_DUMP ? nan 
+    -->
 
-    https://github.com/Lazy974/s2
+    <!-- Step 1 -->
+    <!--
+    Bravo, vous venez de terminer votre initiation à PHP ! Passons à la suite en vous rendant sur ce lien :
+
+    https://github.com/Lazy974/s1
 
     Made with ❤️ by HelloDev
     -->
-
 </body>
 
 </html>
