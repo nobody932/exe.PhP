918d6c86b50865e758878f121e4d362c1a1a0f2a
nobody932 <haykel93370@gmail.com>
2023-11-24 11:50:18 +0100

    update

--- query_sql2/pages/movies/index.php
+++ query_sql2/pages/movies/index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/styles.css">
     <title classname="uppercase ">movies</title>
 </head>
-<body>
+<body class="colorbgg">
     <header>
         <nav>
             <ul>
@@ -21,84 +22,107 @@
             </ul>
         </nav>
     </header>
-    <h1>Movie</h1>
+    <h1 class="text-center">Movie</h1>
 
     <!-- create -->
-    <h2>Create</h2>
-    <form action="../../assets/php/controllers/movies/create_movies.php" method="POST">
-    <div>
-            <label for="movie-title">Titre</label>
-            <input type="text" id="movie-title" name="movie-title">
-        </div> 
-        <div>
-            <label for="movie-url">URL</label>
-            <input type="text" id="movie-url" name="movie-url">
-        </div> 
-        <div>
-            <label for="movie-description">Description</label>
-            <input type="text" id="movie-description" name="movie-description">
-        </div>
-        <div>
-            <label for="movie-country">Pays</label>
-            <input type="text" id="movie-country" name="movie-country">
-        </div>
-    
-        <button type="submit">Envoyer</button>
+    <section class="glasspink self-center">
+        <h2 class="text-center glassblack">Create</h2>
+        <div class="pading self-center">
+            <div class="flex-contain">
+                <form action="../../assets/php/controllers/movies/create_movies.php" method="POST">
+                    <div class="marggin self-center">
+                        <label for="movie-title">Titre</label>
+                        <input type="text" id="movie-title" name="movie-title">
+                    </div> 
+                    <div class="marggin self-center">
+                        <label for="movie-url">URL</label>
+                        <input type="text" id="movie-url" name="movie-url">
+                    </div> 
+                    <div class="marggin self-center">
+                        <label for="movie-description">Description</label>
+                        <input type="text" id="movie-description" name="movie-description">
+                    </div>
+                    <div class="marggin self-center">
+                        <label for="movie-country">Pays</label>
+                        <input type="text" id="movie-country" name="movie-country">
+                    </div>
+                    
+                    <button type="submit">Envoyer</button>
+                </div>
+            </div>
+    </section>
     </form>
+    
+<div class="margin">
+
+</div>
 
     <!-- read -->
+    <section class="contain self-center">
+        <h2 class="text-center glassblack">Apperçus de l'ajouts</h2>
+        <div class="pading self-center">
+            <div class="flex-contain">
+                <?php 
 
-    <h2>Read</h2>
-    <?php 
+require_once('../../assets/php/middleware/connect.php');
 
-    require_once('../../assets/php/middleware/connect.php');
+$query_movies = $db_connect->query('SELECT * FROM movies');
 
-    $query_movies = $db_connect->query('SELECT * FROM movies');
+foreach ($query_movies as $movie) {
+    echo $movie['title'];
+    echo '<br />';
+    echo $movie['url'];
+    echo '<br />';
+}
 
-    foreach ($query_movies as $movie) {
-        echo $movie['title'];
-        echo '<br />';
-        echo $movie['url'];
-        echo '<br />';
-    }
-
-    ?>
-
-    <h2>Update</h2>
-    <!-- update -->
-    <form action="../../assets/php/controllers/movies/update_movies.php" method="POST">
-        <div>
-            <label for="movie-id">Id</label>
-            <input type="text" id="movie-id" name="movie-id">
+?>
+            </div>
         </div>
-        <div>
-            <label for="movie-title">Titre</label>
-            <input type="text" id="movie-title" name="movie-title">
-        </div> 
-        <div>
-            <label for="movie-url">URL</label>
-            <input type="text" id="movie-url" name="movie-url">
-        </div> 
-        <div>
-            <label for="movie-description">Description</label>
-            <input type="text" id="movie-description" name="movie-description">
-        </div>
-        <div>
-            <label for="movie-country">Pays</label>
-            <input type="text" id="movie-country" name="movie-country">
-        </div>
+</section>
+    <div class="margin">
+
+    </div>
+    <section class="glassblue  self-center ">
+        <h1 class="text-center glassblack">Update</h1>
+            <div class="">
+                <form class="flex-container self-center pading" action="../../assets/php/controllers/movies/update_movies.php" method="POST">
+                <div class="marggin self-center">
+                    <label for="movie-id">Id</label>
+                    <input type="text" id="movie-id" name="movie-id">
+                </div>
+                <div class="marggin self-center">
+                    <label for="movie-title">Titre</label>
+                    <input type="text" id="movie-title" name="movie-title">
+                </div> 
+                <div class="marggin self-center">
+                    <label for="movie-url">URL</label>
+                    <input type="text" id="movie-url" name="movie-url">
+                </div> 
+                <div class="marggin self-center">
+                    <label for="movie-description">Description</label>
+                    <input type="text" id="movie-description" name="movie-description">
+            </div>
+            <div class="marggin self-center">
+                <label for="movie-country">Pays</label>
+                <input type="text" id="movie-country" name="movie-country">
+            </div>
+            
+            <button type="submit">Envoyer</button>
+    </form>
+    </section>
     
-        <button type="submit">Envoyer</button>
-    </form>
-
     <!-- delete -->
-    <h2>Delete</h2>
-    <form action="../../assets/php/controllers/movies/delete_movies.php" method="POST">
-        <div>
-            <label for="movie-id">Id</label>
-            <input type="text" id="movie-id" name="movie-id">
+    <section class="glassgrey self-center">
+      <h1 class="text-center glassblack">Delete</h1> 
+        <div class="text-center">
+            <form class="glassgrey self-center pading" action="../../assets/php/controllers/movies/delete_movies.php" method="POST">
+                <div class="marggin self-center">
+                    <label for="movie-id">Id</label>
+                    <input type="text" id="movie-id" name="movie-id">
+                </div>
+                <button type="submit">Envoyer</button>
+            </form>
         </div>
-        <button type="submit">Envoyer</button>
-    </form>
+    </section>
 </body>
 </html>
--- query_sql2/pages/store/index.php
+++ query_sql2/pages/store/index.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="../../assets/css/styles.css">
     <title classname="uppercase">Store</title>
 </head>
-<body class="colorbg, ">
+<body class="colorbg ">
     <header>
         <nav>
             <ul>
@@ -28,85 +28,109 @@
 
         <!-- create -->
         <div class="glassblack">
-             <h1 class="self-center">Create</h1>
+             <h1 class="text-center">Create</h1>
         </div>
-        <form class="flex-container self-center" action="../../assets/php/controllers/store/create_store.php" method="POST">
-            <div class="container-center self-center marggin">
-                <div class="marggin">
+        <form class="flex-container self-center pading" action="../../assets/php/controllers/store/create_store.php" method="POST">
+            <div class="container-center  marggin">
+                <div class="marggin self-center">
                     <label for="store-picture">Photo</label>
                     <input type="text" id="store-picture" name="store-picture">
                 </div> 
-                <div class="marggin">
+                <div class="marggin self-center">
                     <label for="store-name">Nom</label>
                     <input type="text" id="store-name" name="store-name">
                 </div> 
-                <div class="marggin">
+                <div class="marggin self-center">
                     <label for="store-adress">Adresse</label>
                     <input type="text" id="store-adress" name="store-adress">
                 </div>
-                <div class="marggin">
+                <div class="marggin self-center">
                     <label for="store-description">Description</label>
                     <input type="text" id="store-description" name="store-description">
                 </div>
                 <div class="self-center">
-                    <button class="testbutton self-center" type="submit">Envoyer</button>
+                    <button class=" self-center" type="submit">Envoyer</button>
                 </div>
                
             </div>
          </form>
     </section>
+    <div class="margin">
+    </div>
     
     <!-- read -->
-        <h1>Read</h1>
-
-        <?php
+    <section class="glassblue flex-contain self-center">
+    <h1 class="text-center glassblack">Apperçus de l'ajouts</h1>
+        <div class=" pading self-center">
+            <div class="text-center flex-contain">
+                <?php
         
         require_once('../../assets/php/middleware/connect.php');
-
+        
         $query_store = $db_connect->query('SELECT * FROM store');
-
+        
         foreach ($query_store as $store) {
             echo $store['store_name'];
             echo '<br />';
             echo $store['adress'];
             echo '<br />';
         }
+        
+        ?>
+        </div>
+        </div>
+        </section>
 
-    ?>
+        <div class="margin">
+        </div>
+
         <!-- update -->
-    <h1>Update</h1>
-    <form action="../../assets/php/controllers/store/update_store.php" method="POST">
-        <div>
-            <label for="store-id">Id</label>
-            <input type="text" id="store-id" name="store-id">
-        </div>
-        <div>
-            <label for="store-picture">Photo</label>
-            <input type="text" id="store-picture" name="store-picture">
-        </div> 
-        <div>
-            <label for="store-name">Nom</label>
-            <input type="text" id="store-name" name="store-name">
-        </div> 
-        <div>
-            <label for="store-adress">Adresse</label>
-            <input type="text" id="store-adress" name="store-adress">
-        </div>
-        <div>
-            <label for="store-description">Description</label>
-            <input type="text" id="store-description" name="store-description">
-        </div>
-        <button type="submit">Envoyer</button>
-    </form>
-        <!-- delete -->
-        <h1>Delete</h1>
+        <section class="contain  self-center padding">
+        <h1 class="text-center glassblack">Update</h1>
+            <div class="">
+                <form class="flex-container self-center pading" action="../../assets/php/controllers/store/update_store.php" method="POST">
+                    <div class="container-center ">
+                        <div class="marggin self-center">
+                            <label for="store-id">Id</label>
+                            <input type="text" id="store-id" name="store-id">
+                        </div>
+                        <div class="marggin self-center">
+                            <label for="store-picture">Photo</label>
+                            <input type="text" id="store-picture" name="store-picture">
+                        </div> 
+                        <div class="marggin self-center">
+                            <label for="store-name">Nom</label>
+                            <input type="text" id="store-name" name="store-name">
+                        </div> 
+                        <div class="marggin self-center">
+                            <label for="store-adress">Adresse</label>
+                            <input type="text" id="store-adress" name="store-adress">
+                        </div>
+                        <div class="marggin self-center">
+                            <label for="store-description">Description</label>
+                            <input type="text" id="store-description" name="store-description">
+                        </div>
+                        <button type="submit">Envoyer</button>
+                    </form>
+                </div>    
+                </div>
+            </section>
+            <div class="margin">
 
-        <form action="../../assets/php/controllers/store/delete_store.php" method="POST">
-        <div>
-            <label for="store-id">Id</label>
-            <input type="text" id="store-id" name="store-id">
+            </div>
+    <!-- delete -->
+    <section class=" glassblue self-center">
+      <h1 class="text-center glassblack">Delete</h1> 
+        <div class="text-center">
+            
+            <form action="../../assets/php/controllers/store/delete_store.php" method="POST">
+                <div class="marggin self-center">
+                    <label for="store-id">Id</label>
+                    <input type="text" id="store-id" name="store-id">
+                </div>
+                <button type="submit">Envoyer</button>
+            </form>
         </div>
-        <button type="submit">Envoyer</button>
-    </form>
+    </section>
 </body>
 </html>
